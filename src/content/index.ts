81fe4ba34277935c04f15de787f6f74703505853
Tomasz Szpak <tomszp@gmail.com>
2026-01-12 19:40:04 +0100

    add catch-up to latest improvements

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,3 +2,4 @@ export * as manufacturerRepository from './manufacturer.repository';
 export * as categoryRepository from './category.repository';
 export * as productRepository from './product.repository';
 export * as miscRepository from './misc.repository';
+export * as translationRepository from './translation.repository';
---
+++ src/content/translation.repository.ts
@@ -0,0 +1,36 @@
+import { useTranslations } from "../i18n/utils";
+import type { ui } from "../i18n/ui";
+
+export type AvailableLanguage = {
+    code: string;
+    label: string;
+    flag: string;
+}
+
+export function findAvailableLanguages(): AvailableLanguage[] {
+    const languages = [
+        { code: 'en', label: 'EN', flag: '🇬🇧' },
+        { code: 'pl', label: 'PL', flag: '🇵🇱' },
+    ];
+
+    return languages;
+}
+
+export function getLanguage(code: string): AvailableLanguage {
+    const languages = findAvailableLanguages();
+    const language = languages.find((lang) => lang.code === code);
+
+    if (!language) {
+        throw new Error(`Language with code ${code} not found`);
+    }
+
+    return language;
+}
+
+export function getDefaultLanguage(): AvailableLanguage {
+    return { code: 'pl', label: 'PL', flag: '🇵🇱' };
+}
+
+export function getTranslator(lang: string) {
+    return useTranslations(lang as keyof typeof ui);
+}
